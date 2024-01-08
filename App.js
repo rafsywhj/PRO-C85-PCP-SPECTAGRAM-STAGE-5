1b7d11d4be50617fb54bf3c88258112ce7fb580b
vishalgddam873 <vishal18@navgurukul.org>
2024-01-08 13:57:25 +0530

    Code Updated and SDK Version Upated to 49

--- App.js
+++ App.js
@@ -1,41 +1,34 @@
-import * as React from "react";
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { createStackNavigator } from "@react-navigation/stack";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/Register";
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/Register';
 
-import DrawerNavigator from "./navigation/DrawerNavigator";
-
-import * as firebase from "firebase";
-import { firebaseConfig } from "./config";
-
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-} else {
-  firebase.app();
-}
+import DrawerNavigator from './navigation/DrawerNavigator';
+import db from './config';
 
 const Stack = createStackNavigator();
 
 const StackNav = () => {
-  return(
-  <Stack.Navigator initialRouteName="Login"  screenOptions={{
-    headerShown: false,
-    gestureEnabled: false
-  }}>
-    <Stack.Screen name="Login" component={LoginScreen} />
-    <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-    <Stack.Screen name="Dashboard" component={DrawerNavigator} />
-  </Stack.Navigator>)
-}
+	return (
+		<Stack.Navigator
+			initialRouteName='Login'
+			screenOptions={{
+				headerShown: false,
+				gestureEnabled: false,
+			}}>
+			<Stack.Screen name='Login' component={LoginScreen} />
+			<Stack.Screen name='RegisterScreen' component={RegisterScreen} />
+			<Stack.Screen name='Dashboard' component={DrawerNavigator} />
+		</Stack.Navigator>
+	);
+};
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <StackNav/>
-    </NavigationContainer>)
-
+	return (
+		<NavigationContainer>
+			<StackNav />
+		</NavigationContainer>
+	);
 }
-
